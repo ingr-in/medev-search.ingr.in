272abb6e1124a2edd4bc07450aea0d0c7559acbd
ingr.in <aco@ingr.in>
2026-03-22 21:21:22 +0530

    Update app.py

--- public/app.py
+++ public/app.py
@@ -1,1 +1,10 @@
-print'hellow';
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Hello World</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
