585cd97d6856b8cfe802c81e157131a950ac5b2d
ingr.in <aco@ingr.in>
2026-03-23 12:00:37 +0530

    Update app.py

--- public/app.py
+++ public/app.py
@@ -6,8 +6,8 @@ from routes.api import api
 app = Flask(__name__)
 
 # Register routes
-app.register_blueprint(main_routes)
-app.register_blueprint(api_routes, url_prefix="/api")
+app.register_blueprint(main)
+app.register_blueprint(api, url_prefix="/api")
 
 if __name__ == '__main__':
     app.run(host='https://python.ingr.in/')
