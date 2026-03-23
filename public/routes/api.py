05ce25d6b580af5688ea0c967196bcf22e8e5d72
ingr.in <aco@ingr.in>
2026-03-23 11:49:59 +0530

    Update api.py

--- public/routes/api.py
+++ public/routes/api.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, jsonify
 from services.api_service import get_data
 
-api_routes = Blueprint("api", __name__)
+api = Blueprint("api", __name__)
 
-@api_routes.route("/data")
+@api.route("/data")
 def data():
     result = get_data()
     return jsonify(result)
