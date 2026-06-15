db9ffe29384b6d0bf6e7f7db302d14a998d3b00f
ingr.in <aco@ingr.in>
2026-06-15 11:18:36 +0530

    Refactor app.py to include blueprints
    Added blueprint registrations for main, api, and bots.

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+
 from spy.man.main import main
 from spy.man.api import api
 from spy.man.bots import bots
-from flask import Flask, render_template
 
 app = Flask(
     __name__,
-    template="spy/templates",
-    static="spy/static",
-    main="spy/main/",
-    api="spy/api",
-    bots="spy/bots"
+    template_folder="spy/templates",
+    static_folder="spy/static"
 )
+
+# Blueprints Register
+app.register_blueprint(main)
+app.register_blueprint(api, url_prefix="/api")
+app.register_blueprint(bots, url_prefix="/bots")
 
 @app.route("/")
 def home():
@@ -21,7 +23,7 @@ def home():
 @app.route("/spy")
 def spy():
     return {
-        "message": "pydir working"
+        "message": "spy working"
     }
 
 if __name__ == "__main__":
